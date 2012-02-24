d0929d7338cdf0c87bee20a2755fbee2d1743e7c
David Way <david.way12@gmail.com>
2012-02-24 13:18:01 -0500

    added serverConnectionController files

---
+++ LabelMe/ServerConnectionController.h
@@ -0,0 +1,13 @@
+//
+//  ServerConnectionController.h
+//  LabelMe
+//
+//  Created by David Way on 2/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ServerConnectionController : NSObject
+
+@end
---
+++ LabelMe/ServerConnectionController.m
@@ -0,0 +1,13 @@
+//
+//  ServerConnectionController.m
+//  LabelMe
+//
+//  Created by David Way on 2/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ServerConnectionController.h"
+
+@implementation ServerConnectionController
+
+@end
